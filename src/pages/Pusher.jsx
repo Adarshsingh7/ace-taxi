ca61d950b31420a954351d9b29a5072016f08b4e
ace-adarsh <officialadarsh2023@gmail.com>
2024-07-10 18:02:48 +0530

    make structure of the map

--- src/pages/Pusher.jsx
+++ src/pages/Pusher.jsx
@@ -10,6 +10,7 @@ import PageNotFound from './PageNotFound';
 
 export default function Pusher() {
 	const { callerTab, insertValue } = useBooking();
+	console.log(callerTab);
 	const [value, setValue] = useState(
 		callerTab.length - 1 >= 0 ? callerTab.length - 1 : 0
 	);
@@ -24,47 +25,58 @@ export default function Pusher() {
 
 	return (
 		<Box
-			sx={{
-				margin: '1vh auto',
-				width: {
-					xs: '100%',
-					sm: '95%',
-					md: '80%',
-					lg: '45%',
-					xl: '45%',
-				},
-				borderColor: '#e5e7eb',
-				borderWidth: '1px',
-			}}
+			className='flex justify-between'
+			sx={{ width: '100%' }}
 		>
-			<Tabs
-				value={value}
-				sx={{ backgroundColor: '#e5e7eb' }}
-				onChange={handleChange}
-				variant='scrollable'
-				scrollButtons
-				allowScrollButtonsMobile
-				aria-label='scrollable force tabs example'
+			<Box
+				sx={{
+					margin: '1vh auto',
+					width: '50%', // Adjusting the width to 50% for the first child Box
+					borderColor: '#e5e7eb',
+					borderWidth: '1px',
+				}}
 			>
-				{callerTab.map((item, index) => (
-					<Tab
-						label={`Caller ${index + 1}`}
-						key={index}
-					/>
-				))}
-			</Tabs>
+				<Tabs
+					value={value}
+					sx={{ backgroundColor: '#e5e7eb' }}
+					onChange={handleChange}
+					variant='scrollable'
+					scrollButtons
+					allowScrollButtonsMobile
+					aria-label='scrollable force tabs example'
+				>
+					{callerTab.map((item, index) => (
+						<Tab
+							label={index === 0 ? 'New' : item.PhoneNumber}
+							key={index}
+						/>
+					))}
+				</Tabs>
 
-			<Box>
-				{callerTab.length === 0 ? (
-					<PageNotFound message='No New Callers Found' />
-				) : (
-					<Booking
-						bookingData={callerTab[value]}
-						key={value}
-						insertValue={insertValue}
-						id={value}
-					/>
-				)}
+				<Box>
+					{callerTab.length === 0 ? (
+						<PageNotFound message='No New Callers Found' />
+					) : (
+						<Booking
+							bookingData={callerTab[value]}
+							key={value}
+							insertValue={insertValue}
+							id={value}
+						/>
+					)}
+				</Box>
+			</Box>
+			<Box
+				className='w-[50vw]'
+				sx={{ width: '50%', padding: 1 }}
+			>
+				<div className='flex gap-1 flex-col'>
+					<div className='w-full h-[40vh] bg-gray-400 m-auto'>map</div>
+					<div className='flex justify-between w-full h-full gap-1'>
+						<div className='w-full h-full bg-pink-400'>part 1</div>
+						<div className='w-full h-full bg-green-400'>part 2</div>
+					</div>
+				</div>
 			</Box>
 		</Box>
 	);
